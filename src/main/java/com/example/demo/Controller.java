2a455c04a832fa6cabf8ea3d0c67282cf086dc22
Ahmad Zulfadli <ahmadzulfadli342@gmail.com>
2026-07-14 20:08:18 +0700

    chore: change path

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/demo/")
 public class Controller {
-    @GetMapping("get-demo3")
+    @GetMapping("get-demo-3")
     public ResponseEntity<DemoModel> getTest() {
         DemoModel demoModel = new DemoModel();
         demoModel.setMessage("Hallo, from demo3");
